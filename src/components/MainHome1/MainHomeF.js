4efc9692311ba1aadc43a471596c6b4dcc01f023
unknown <abdosker11@yahoo.com>
2022-09-21 01:10:14 +0200

    first-try-raml

--- src/components/MainHome1/MainHomeF.js
+++ src/components/MainHome1/MainHomeF.js
@@ -7,7 +7,67 @@ const MainHomeF = () => {
     <>
     <JustText />
 
-<section>
+<section className='MainHomeFExtends'>
+
+<div>
+    <div className='OpenOnHoverMain'>
+    <svg xmlns="http://www.w3.org/2000/svg" width="178.381" height="266.723" viewBox="0 0 178.381 266.723">
+  <path id="Rectangle_1" data-name="Rectangle 1" d="M120.983,0h0a48.393,48.393,0,0,1,48.393,48.393v91.594A120.983,120.983,0,0,1,48.393,260.971h0A48.393,48.393,0,0,1,0,212.578V120.983A120.983,120.983,0,0,1,120.983,0Z" transform="translate(9.108) rotate(2)" fill="#379fcf"/>
+</svg>
+</div>
+</div>
+
+<div>
+    <div className='OpenOnHoverMain'>
+    <svg xmlns="http://www.w3.org/2000/svg" width="182.436" height="273.3" viewBox="0 0 182.436 273.3">
+  <path id="Rectangle_2" data-name="Rectangle 2" d="M123.72,0h0a49.488,49.488,0,0,1,49.488,49.488V143.7a123.72,123.72,0,0,1-123.72,123.72h0A49.488,49.488,0,0,1,0,217.93V123.72A123.72,123.72,0,0,1,123.72,0Z" transform="translate(9.333) rotate(2)" fill="#f8c459"/>
+</svg>
+
+</div>
+</div>
+
+
+<div>
+    <div className='OpenOnHoverMain'>
+    <svg xmlns="http://www.w3.org/2000/svg" width="179.714" height="269.918" viewBox="0 0 179.714 269.918">
+  <path id="Rectangle_3" data-name="Rectangle 3" d="M121.858,0h0A48.743,48.743,0,0,1,170.6,48.743v93.525A121.858,121.858,0,0,1,48.743,264.125h0A48.743,48.743,0,0,1,0,215.382V121.858A121.858,121.858,0,0,1,121.858,0Z" transform="translate(9.218) rotate(2)" fill="#5eb253"/>
+</svg>
+</div>
+<div className='OpenOnHover'>
+<svg xmlns="http://www.w3.org/2000/svg" width="186.455" height="388.4" viewBox="0 0 186.455 388.4">
+  <path id="Rectangle_12" data-name="Rectangle 12" d="M123.72,0h0a49.488,49.488,0,0,1,49.488,49.488v209.38a123.72,123.72,0,0,1-123.72,123.72h0A49.488,49.488,0,0,1,0,333.1V123.72A123.72,123.72,0,0,1,123.72,0Z" transform="translate(13.352) rotate(2)" fill="#5eb253" opacity="0.498"/>
+</svg>
+</div>
+</div>
+
+
+
+<div>
+    <div className='OpenOnHoverMain'>
+    <svg xmlns="http://www.w3.org/2000/svg" width="178.381" height="266.723" viewBox="0 0 178.381 266.723">
+  <path id="Rectangle_4" data-name="Rectangle 4" d="M120.983,0h0a48.393,48.393,0,0,1,48.393,48.393v91.594A120.983,120.983,0,0,1,48.393,260.971h0A48.393,48.393,0,0,1,0,212.578V120.983A120.983,120.983,0,0,1,120.983,0Z" transform="translate(9.108) rotate(2)" fill="#379fcf"/>
+</svg>
+</div>
+<div className='OpenOnHover'>
+<svg xmlns="http://www.w3.org/2000/svg" width="182.609" height="278.26" viewBox="0 0 182.609 278.26">
+  <path id="Rectangle_15" data-name="Rectangle 15" d="M123.72,0h0a49.488,49.488,0,0,1,49.488,49.488v99.173a123.72,123.72,0,0,1-123.72,123.72h0A49.488,49.488,0,0,1,0,222.893V123.72A123.72,123.72,0,0,1,123.72,0Z" transform="translate(9.506) rotate(2)" fill="#379fcf" opacity="0.503"/>
+</svg>
+</div>
+</div>
+
+<div>
+    <div className='OpenOnHoverMain'>
+    <svg xmlns="http://www.w3.org/2000/svg" width="182.436" height="273.3" viewBox="0 0 182.436 273.3">
+  <path id="Rectangle_5" data-name="Rectangle 5" d="M123.72,0h0a49.488,49.488,0,0,1,49.488,49.488V143.7a123.72,123.72,0,0,1-123.72,123.72h0A49.488,49.488,0,0,1,0,217.93V123.72A123.72,123.72,0,0,1,123.72,0Z" transform="translate(9.333) rotate(2)" fill="#f8c459"/>
+</svg>
+</div>
+<div className='OpenOnHover'>
+<svg xmlns="http://www.w3.org/2000/svg" width="184.568" height="334.353" viewBox="0 0 184.568 334.353">
+  <path id="Rectangle_16" data-name="Rectangle 16" d="M123.72,0h0a49.488,49.488,0,0,1,49.488,49.488v155.3a123.72,123.72,0,0,1-123.72,123.72h0A49.488,49.488,0,0,1,0,279.02V123.72A123.72,123.72,0,0,1,123.72,0Z" transform="translate(11.465) rotate(2)" fill="#f8c459" opacity="0.499"/>
+</svg>
+</div>
+</div>
+
 <div>
     <div className='OpenOnHoverMain'>
         <svg  xmlns="http://www.w3.org/2000/svg" width="179.714" height="269.918" viewBox="0 0 179.714 269.918">
@@ -18,22 +78,6 @@ const MainHomeF = () => {
 <svg  xmlns="http://www.w3.org/2000/svg" width="183.064" height="291.294" viewBox="0 0 183.064 291.294">
   <path id="Rectangle_17" data-name="Rectangle 17" d="M123.72,0h0a49.488,49.488,0,0,1,49.488,49.488V161.7a123.72,123.72,0,0,1-123.72,123.72h0A49.488,49.488,0,0,1,0,235.935V123.72A123.72,123.72,0,0,1,123.72,0Z" transform="translate(9.961) rotate(2)" fill="#5eb253" opacity="0.497"/>
 </svg>
-</div>
-</div>
-
-
-<div>
-    <div className='OpenOnHoverMain'>
-    <svg xmlns="http://www.w3.org/2000/svg" width="182.436" height="273.3" viewBox="0 0 182.436 273.3">
-  <path id="Rectangle_5" data-name="Rectangle 5" d="M123.72,0h0a49.488,49.488,0,0,1,49.488,49.488V143.7a123.72,123.72,0,0,1-123.72,123.72h0A49.488,49.488,0,0,1,0,217.93V123.72A123.72,123.72,0,0,1,123.72,0Z" transform="translate(9.333) rotate(2)" fill="#f8c459"/>
-</svg>
-
-</div>
-<div className='OpenOnHover'>
-<svg xmlns="http://www.w3.org/2000/svg" width="184.568" height="334.353" viewBox="0 0 184.568 334.353">
-  <path id="Rectangle_16" data-name="Rectangle 16" d="M123.72,0h0a49.488,49.488,0,0,1,49.488,49.488v155.3a123.72,123.72,0,0,1-123.72,123.72h0A49.488,49.488,0,0,1,0,279.02V123.72A123.72,123.72,0,0,1,123.72,0Z" transform="translate(11.465) rotate(2)" fill="#f8c459" opacity="0.499"/>
-</svg>
-
 </div>
 </div>
 
